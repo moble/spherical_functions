b593233921217fe14da0c236a2ea5d02fe90e261
Michael Boyle <michael.oliver.boyle@gmail.com>
2014-11-07 23:59:20 -0500

    Expand some tests

--- test_spherical_functions.py
+++ test_spherical_functions.py
@@ -16,14 +16,9 @@ import pytest
 from operator import mul    # or mul=lambda x,y:x*y
 from fractions import Fraction
 from functools import reduce
+import random
 
 import numba # This is to check to make sure we're actually using numba
-
-special_angles = np.arange(-8*np.pi, 8*np.pi+0.1, np.pi/4.)
-
-@pytest.fixture
-def special_orientations():
-    return np.array([[alpha,beta,gamma] for gamma in special_angles for beta in special_angles for alpha in special_angles])
 
 def nCk(n,k):
     """Simple binomial function"""
@@ -86,13 +81,56 @@ def test_Wigner3j():
                                        -(-1)**(j1+j2+j3)*sp.Wigner3j((j2+j3+m1)//2,(j1+j3+m2)//2,(j1+j2-m1-m2)//2,
                                                                      j1-(j2+j3-m1)//2,j2-(j1+j3-m2)//2,j3-(j1+j2+m1+m2)//2)) < 1.e-15
 
-def test_WignerD():
+@pytest.fixture
+def Rs():
+    ones = [0,-1.,1.]
+    rs = [np.quaternion(w,x,y,z).normalized() for w in ones for x in ones for y in ones for z in ones][1:]
+    random.seed(1842)
+    rs = rs + [r.normalized() for r in [np.quaternion(random.uniform(-1,1), random.uniform(-1,1),
+                                                      random.uniform(-1,1), random.uniform(-1,1) ) for i in range(20)]]
+    return np.array(rs)
+
+precision_WignerD = 3.e-14
+
+def test_WignerD_negative_argument(Rs, ell_max):
+    # For integer ell, D(R)=D(-R)
+    LMpM = np.array([[ell,mp,m] for ell in range(ell_max+1) for mp in range(-ell,ell+1) for m in range(-ell,ell+1)])
+    for R in Rs:
+        print(R)
+        assert np.allclose( sp.WignerD(R, LMpM), sp.WignerD(-R, LMpM), rtol=precision_WignerD)
+
+def test_WignerD_representation_property(Rs,ell_max):
     # Test the representation property for special and random angles
+    # Try half-integers, too
+    LMpM = np.array([[ell,mp,m] for ell in range(ell_max+1) for mp in range(-ell,ell+1) for m in range(-ell,ell+1)])
+    for R1 in Rs:
+        for R2 in Rs:
+            D1 = sp.WignerD(R1, LMpM)
+            D2 = sp.WignerD(R2, LMpM)
+            D12 = np.array([np.sum([D1[sp._Wigner_index(ell,mp,mpp)]*D2[sp._Wigner_index(ell,mpp,m)] for mpp in range(-ell,ell+1)])
+                            for ell in range(ell_max+1) for mp in range(-ell,ell+1) for m in range(-ell,ell+1)])
+            assert np.allclose( sp.WignerD(R1*R2, LMpM), D12, atol=precision_WignerD)
+
+def test_WignerD_symmetries(Rs, ell_max):
     # Test symmetries
-    # Test rotations about the z axis
-    # Test various rotations onto the -z axis
-    # Test the special angles
-    assert sp.WignerD(quaternion.one, 2, 2, 2) == 1.0
+    pass
+
+def test_WignerD_roundoff_cases(Rs,ell_max):
+    LMpM = np.array([[ell,mp,m] for ell in range(ell_max+1) for mp in range(-ell,ell+1) for m in range(-ell,ell+1)])
+    # Test rotations with |Ra|<1e-15
+    expected = [((-1)**ell if mp==-m else 0.0) for ell in range(ell_max+1) for mp in range(-ell,ell+1) for m in range(-ell,ell+1)]
+    assert np.allclose( sp.WignerD(quaternion.x, LMpM), expected, rtol=precision_WignerD)
+    expected = [((-1)**(ell+m) if mp==-m else 0.0) for ell in range(ell_max+1) for mp in range(-ell,ell+1) for m in range(-ell,ell+1)]
+    assert np.allclose( sp.WignerD(quaternion.y, LMpM), expected, rtol=precision_WignerD)
+    # Test rotations with |Rb|<1e-15
+    expected = [(1.0 if mp==m else 0.0) for ell in range(ell_max+1) for mp in range(-ell,ell+1) for m in range(-ell,ell+1)]
+    assert np.allclose( sp.WignerD(quaternion.one, LMpM), expected, rtol=precision_WignerD)
+    expected = [((-1)**m if mp==m else 0.0) for ell in range(ell_max+1) for mp in range(-ell,ell+1) for m in range(-ell,ell+1)]
+    assert np.allclose( sp.WignerD(quaternion.z, LMpM), expected, rtol=precision_WignerD)
+    # Test rotations with |Ra|~1e-10 and large ell
+    pass
+    # Test rotations with |Rb|~1e-10 and large ell
+    pass
 
 if __name__=='__main__':
     print("This script is intended to be run through py.test")
