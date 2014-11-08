75866f1e6f7a9d976d103b7249c928472a52393b
Michael Boyle <michael.oliver.boyle@gmail.com>
2014-11-08 18:13:49 -0500

    Working; slight simplification of loops

--- WignerD.py
+++ WignerD.py
@@ -6,7 +6,7 @@ from . import (_Wigner_coefficient as coeff, binomial_coefficient,
                epsilon, min_exp, mant_dig, error_on_bad_indices)
 from quaternion.numba_wrapper import njit, jit, int64, xrange
 
-epsilon_squared = epsilon**2
+epsilon_squared = epsilon*epsilon
 
 _log2 = np.log(2)
 
@@ -149,11 +149,11 @@ def _WignerD(Ra, Rb, indices, elements):
 
     # These constants are the recurring quantities in the computation
     # of the matrix elements, so we calculate them here just once
-    absRa_squared = float(Ra*Ra.conjugate())
-    absRb_squared = float(Rb*Rb.conjugate())
-    absRRatioSquared = (-absRb_squared/absRa_squared if absRa_squared>=absRb_squared else -absRa_squared/absRb_squared)
+    absRa = abs(Ra)
+    absRb = abs(Rb)
+    absRRatioSquared = (-absRb*absRb/(absRa*absRa) if absRa>=absRb else -absRa*absRa/(absRb*absRb))
 
-    if(absRa_squared<=epsilon_squared):
+    if(absRa<=epsilon):
         for i in xrange(N):
             ell,mp,m = indices[i,0:3]
             if(mp!=-m or abs(mp)>ell or abs(m)>ell):
@@ -164,7 +164,7 @@ def _WignerD(Ra, Rb, indices, elements):
                 else:
                     elements[i] = -Rb**(2*m)
 
-    elif(absRb_squared<=epsilon_squared):
+    elif(absRb<=epsilon):
         for i in xrange(N):
             ell,mp,m = indices[i,0:3]
             if(mp!=m or abs(mp)>ell or abs(m)>ell):
@@ -172,13 +172,13 @@ def _WignerD(Ra, Rb, indices, elements):
             else:
                 elements[i] = Ra**(2*m)
 
-    elif(absRa_squared<absRb_squared):
+    elif(absRa<absRb):
         for i in xrange(N):
             ell,mp,m = indices[i,0:3]
             if(abs(mp)>ell or abs(m)>ell):
                 elements[i] = 0.0j
             else:
-                Prefactor = coeff(ell, mp, m) * absRb_squared**(ell-m) * Rb**(m-mp) * Ra**(m+mp)
+                Prefactor = coeff(ell, mp, m) * absRb**(2*ell-2*m) * Rb**(m-mp) * Ra**(m+mp)
                 if((ell+m)%2!=0):
                     Prefactor *= -1
                 if(Prefactor==0.0j):
@@ -192,13 +192,13 @@ def _WignerD(Ra, Rb, indices, elements):
                                  + Sum * absRRatioSquared )
                     elements[i] = Prefactor * Sum * absRRatioSquared**rhoMin
 
-    else: # absRa_squared >= absRb_squared
+    else: # absRa >= absRb
         for i in xrange(N):
             ell,mp,m = indices[i,0:3]
             if(abs(mp)>ell or abs(m)>ell):
                 elements[i] = 0.0j
             else:
-                Prefactor = coeff(ell, mp, m) * absRa_squared**(ell-m) * Ra**(m+mp) * Rb**(m-mp)
+                Prefactor = coeff(ell, mp, m) * absRa**(2*ell-2*m) * Ra**(m+mp) * Rb**(m-mp)
                 if(Prefactor==0.0j):
                     elements[i] = 0.0j
                 else:
