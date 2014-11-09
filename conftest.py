dee8967d178ae9e5e3d6b13f23f8393dedddcbf6
Michael Boyle <michael.oliver.boyle@gmail.com>
2014-11-09 18:36:30 -0500

    Default to using full ell_max in tests

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 import pytest
+from spherical_functions import ell_max as ell_max_default
 
 def pytest_addoption(parser):
-    parser.addoption("--ell_max", action="store", type=int, default=4,
+    parser.addoption("--ell_max", action="store", type=int, default=ell_max_default,
                      help="Maximum ell value to test")
 
 @pytest.fixture
