aef64d59b34e4485b79812f6e623c17d28d27324
Michael Boyle <michael.oliver.boyle@gmail.com>
2019-09-18 17:08:27 -0400

    Work around pytest change to markers

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,8 +11,21 @@ from spherical_functions import ell_max as ell_max_default
 def pytest_addoption(parser):
     parser.addoption("--ell_max", action="store", type=int, default=ell_max_default,
                      help="Maximum ell value to test")
-    parser.addoption("--run_slow_tests", action="store_true",
+    parser.addoption("--run_slow_tests", action="store_true", default=False,
                      help="Run all tests, including slow ones")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "slow: marks tests as slow")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--run_slow_tests"):
+        return
+    skip_slow = pytest.mark.skip(reason="need --run_slow_tests option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
 
 
 def pytest_runtest_setup(item):
